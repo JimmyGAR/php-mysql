63945c73544c3b647a0f0f0d558bea07dc936733
JimmyGAR <jimmy.garnier1110@gmail.com>
2025-09-02 16:09:01 +0200

    Page de contact

--- header.php
+++ header.php
@@ -2,9 +2,8 @@
     <div class="element_menu">
         <h3>Titre menu</h3>
         <ul>
-            <li><a href="page1.html">Lien</a></li>
-            <li><a href="page2.html">Lien</a></li>
-            <li><a href="page3.html">Lien</a></li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="contact.php">Contact</a></li>
         </ul>
     </div>
 </nav>
