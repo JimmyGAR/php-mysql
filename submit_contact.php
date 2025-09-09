5632551763dd2bfcb70f7a56aaa7f76286cebb0d
JimmyGAR <jimmy.garnier1110@gmail.com>
2025-09-09 10:31:44 +0200

    Testez la présence d'un paramètre

--- submit_contact.php
+++ submit_contact.php
@@ -11,6 +11,13 @@
 </head>
 
 <body class="d-flex flex-column min-vh-100">
+    <?php
+    if (!isset($_GET['email']) || !isset($_GET['message'])) {
+        echo ('<h1>Il faut un email et un message pour soumettre le formulaire.</h1>');
+        // Arrête l'exécution de PHP
+        return;
+    }
+    ?>
     <h1>Message bien reçu !</h1>
     <div class="card">
         <div class="card-body">
