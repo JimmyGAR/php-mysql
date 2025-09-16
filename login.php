5955611386388d5cb3bf90563eb208dbed8f2a73
JimmyGAR <jimmy.garnier1110@gmail.com>
2025-09-16 11:43:20 +0200

    Amélioration du CSS pour le login

--- login.php
+++ login.php
@@ -24,7 +24,7 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
             </div>
         <?php endif; ?>
         <div class="mb-3">
-            <label for="email" class="form-label">Email</label>
+            <label for="email" class="form-label">Email<br></label>
             <input type="email" class="form-control" id="email" name= "email"
             aria-describedby="email-help" placeholder="you@exemple.com">
             <div id="email-help" class="form-text">
@@ -32,7 +32,7 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
             </div>
         </div>
         <div class="mb-3">
-            <label for="password" class="form-label">Mot de passe</label>
+            <label for="password" class="form-label">Mot de passe<br></label>
             <input type="password" class="form-control" id="password" name= "password" >
         </div>
         <button type="submit" class="btn btn-primary">Envoyer</button>
